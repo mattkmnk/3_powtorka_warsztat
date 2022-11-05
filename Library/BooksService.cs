b91ac8538025a9e40111cc5db221fb3886583647
Matthew Kaminski <mattkmnk@gmail.com>
2022-11-05 15:33:16 +0100

    Dodaj pliki projektów.

---
+++ Library/BooksService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class BooksService
+    {
+        public void AddBook()
+        {
+            Console.WriteLine("==== Dodawanie Ksiazki ====");
+            Console.Write("Tytul: ");
+            var title = Console.ReadLine();
+            Console.Write("Autor: ");
+            var author = Console.ReadLine();
+            Console.Write("ISBN: ");
+            var ISBN = Console.ReadLine();
+            Console.Write("Rok publikacji: ");
+            var publicationYear = Console.ReadLine();
+            Console.Write("Dostepnych: ");
+            var productAvailable = Console.ReadLine();
+            Console.Write("Cena: ");
+            var price = Console.ReadLine();
+        }
+
+        public void RemoveBook()
+        {
+            Console.WriteLine("==== Usuwanie Ksiazki =====");
+            Console.Write("Tytul: ");
+            var title = Console.ReadLine();
+        }
+
+        public void ListBooks()
+        {
+            Console.WriteLine("==== Lista Ksiazek ====");
+        }
+
+        public void ChangeState()
+        {
+            Console.WriteLine("==== Zmiana Stanu ====");
+            Console.Write("Tytul: ");
+            var title = Console.ReadLine();
+            Console.Write("Zmiana: ");
+            var change = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,104 @@
+﻿using ConsoleApp;
+using Library.Domain;
+using Persistence;
+using System;
+using System.Threading;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if(login == "Admin" && password == "password")
+            {
+                Console.WriteLine("Access Granted");
+            } 
+            else 
+            {
+                Console.WriteLine("Access Denied");
+            }
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+
+            bool AppRunning = true;
+            BooksService service = new BooksService();
+
+
+            while (AppRunning)
+            {
+                PrintMenu();
+                Console.Write("Command: ");
+                string command = Console.ReadLine();
+                switch(command)
+                {
+                    case "dodaj":
+                        HandleCommand(service.AddBook);
+                        break;
+                    case "usun":
+                        HandleCommand(service.RemoveBook);
+                        break;
+                    case "wypisz":
+                        HandleCommand(service.ListBooks);
+                        break;
+                    case "zmien":
+                        HandleCommand(service.ChangeState);
+                        break;
+                    case "dodaj zamowienie":
+                        HandleCommand("proba dodania nowego zamowienia");
+                        break;
+                    case "lista zamowien":
+                        HandleCommand("proba wypisania wszystkich zamowien");
+                        break;
+                    case "wyjdz":
+                        Console.Clear();
+                        Console.WriteLine("Bye!");
+                        AppRunning = false;
+                        break;
+                    default:
+                        HandleCommand("Komenda nie jest wspierana");
+                        break;
+                }
+            }
+        }
+
+        private static void HandleCommand(string output)
+        {
+            Console.Clear();
+            Console.WriteLine(output);
+            Console.WriteLine("Press AnyKey...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void HandleCommand(Action func)
+        {
+            Console.Clear();
+            func();
+            Console.WriteLine("Press AnyKey...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Commands: ");
+            Console.WriteLine("dodaj");
+            Console.WriteLine("usun");
+            Console.WriteLine("wypisz");
+            Console.WriteLine("zmien");
+            Console.WriteLine("dodaj zamowienie");
+            Console.WriteLine("lista zamowien");
+        }
+    }
+}
